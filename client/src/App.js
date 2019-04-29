63baccd1b7f88505074251f91f5a9af1a20bb152
peng.shao <peng.shao@ocado.com>
2019-04-29 18:34:19 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+
+
+class App extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = { apiResponse: "" };
+  }
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }));
+  }
+  componentWillMount() {
+      this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <p className="App-intro">{this.state.apiResponse}</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
